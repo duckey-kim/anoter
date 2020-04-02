b2f1479e1c4428aff292b10e069fc2cd8fd79711
duckey-kim <57125670+duckey-kim@users.noreply.github.com>
2020-04-02 18:16:26 +0900

    nav 1 to 4 chanege
    excample))
    <a href="index.html">
    ->
    <a href="index">

---
+++ sites/server.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs = require('fs');
+const index = fs.readFileSync('index.html');
+const host = "127.0.0.1";
+const port = 3000;
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+  if (req.url == '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    console.log(req.url + `is received`);
+    res.end(index);
+  } else {
+    // let request_path= req.url.slice(1,req.url.length);
+    // console.log(request_path);
+    let req_path = url.parse(req.url).pathname;
+    // req_path=> /***
+    // console.log(req_path);
+    fs.readFile(req_path.substr(1) + '.html', (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        console.log('err');
+        res.end();
+      } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(data);
+        console.log('success');
+      }
+    });
+  }
+});
+server.listen(port, host, () => {
+  console.log(`server is running at http://${host}:${port}/`);
+});
