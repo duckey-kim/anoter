da502a436b7e559382f79d39c27c928324a2cae4
duckey-kim <57125670+duckey-kim@users.noreply.github.com>
2020-04-11 16:51:34 +0900

    expressjs를 활용하여 index.js구성
     - app.get()을 활용하여 path를 통한 request를 통해 @@.html을 연결한다.
     - ejs를 통한 view engine을 설정한다.
     - res.render를 통해 .html파일을 렌더링한다.
     - express의 구조를 지키기 위한 폴더명 변경
     - asset 폴더 -> public
     - pages 폴더 -> views

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require("express");
-const app = express()
+const app = express();
 const port = process.env.PORT || 3000;
+const ejs = require("ejs");
+// 필요한 module require
 
-app.get('/', (req, res) => {
-  res.send("Hi");
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+app.use(express.static(__dirname));
+//view engine을 html로 설정
+//ejs로 render
+//현재디렉토리를 static으로 설정
+//render는  views폴더 안에 있는 html을 읽는다.
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/nav1", (req, res) => {
+  res.render("nav1");
+});
+app.get("/nav2", (req, res) => {
+  res.render("nav2");
+});
+app.get("/nav3", (req, res) => {
+  res.render("nav3");
+});
+app.get("/nav4", (req, res) => {
+  res.render("nav4");
+});
+app.get("/signUp", (req, res) => {
+  res.render("signUp");
+});
+app.get("/write", (req, res) => {
+  res.render("write");
 });
 
 app.listen(port, () => {
