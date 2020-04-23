3599813fa4100edfd325a5ace13af96d4e2399bc
duckey-kim <57125670+duckey-kim@users.noreply.github.com>
2020-04-23 22:56:14 +0900

    Set API option for index.js  - Path : `/` => res.render('index') `/signUp`=> res.render('boards/signUp');  - Path Param : `/boards/:category` =>res.render('boards/'+req.params.category) category : humor , gossip  - Query Param : `/boards/:category/post?action=xxx&number= xxx` action : new_post, post, edit_post number : postNumber => res.render('boards/'+req.query.action)

--- index.js
+++ index.js
@@ -4,36 +4,34 @@ const port = process.env.PORT || 3000;
 const ejs = require("ejs");
 // 필요한 module require
 
-app.set('view engine', 'html');
-app.engine('html', ejs.renderFile);
-app.use(express.static(__dirname));
+app.set("view engine", "html");
+app.engine("html", ejs.renderFile);
+app.use("/public", express.static(__dirname + "/public"));
 //view engine을 html로 설정
 //ejs로 render
 //현재디렉토리를 static으로 설정
 //render는  views폴더 안에 있는 html을 읽는다.
 
+// "/"request "duck-craft/views/"  index.html render
 app.get("/", (req, res) => {
   res.render("index");
 });
-app.get("/boards/humor", (req, res) => {
-  res.render("board1");
-});
-app.get("/boards/gossip", (req, res) => {
-  res.render("board2");
-});
-app.get("boards/humor/posts/new", (req, res) => {
-  res.render("new_post");
-});
-app.get("boards/gossip/posts/new", (req, res) => {
-  res.render("new_post");
+app.get("/signUp", (req, res) => {
+  res.render("boards/signUp");
 });
 
-app.get("boards/gossip/posts/new", (req, res) => {
-  res.render("new_post");
+// "/boards/:category"request "duck-craft/views/"+boards/category.html render
+app.get("/boards/:category", (req, res) => {
+  res.render("boards/" + req.params.category);
 });
-app.get("/write", (req, res) => {
-  res.render("write");
+// "/boards/:category/posts/new"request "duck-craft/views/"+boards/new_post.html render
+// category: humor , gossip
+app.get("/boards/:category/posts", (req, res) => {
+  res.render("boards/" + req.query.action);
 });
+//"boards/:catogory/posts?action=new_post" =>"boards/"+req.query.action
+//"boards/:catogory/posts?action=edit_post" =? Same
+//"boards/:catogory/posts?action=post ""  => Same
 
 app.listen(port, () => {
   console.log(`server is running at http://localhost:${port}/`);
