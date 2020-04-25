ab464442e0b928131ead58f564a4f791166a9842
duckey-kim <57125670+duckey-kim@users.noreply.github.com>
2020-04-25 17:08:54 +0900

    Add ejs render  - 변수설정 : ejs를 사용하여 서버로 부터 받은 데이터를 적용할 수 있는 변수를 설정  - 미정된 변수들 : 서버로 부터 받아올 데이터들을 설정하지 않았기때문에 그를 대신한 text를 입력함

--- index.js
+++ index.js
@@ -4,30 +4,52 @@ const port = process.env.PORT || 3000;
 const ejs = require("ejs");
 // 필요한 module require
 
-app.set("view engine", "html");
+app.set("view engine", "ejs");
 app.engine("html", ejs.renderFile);
 app.use("/public", express.static(__dirname + "/public"));
-//view engine을 html로 설정
+//view engine을 ejs로 설정
 //ejs로 render
 //현재디렉토리를 static으로 설정
 //render는  views폴더 안에 있는 html을 읽는다.
 
 // "/"request "duck-craft/views/"  index.html render
 app.get("/", (req, res) => {
-  res.render("boards/index");
+  res.render("boards/index", {
+    titlename: "Duck-Craft",
+    postTitle: "postTitle",
+    postUser: "postUser",
+    postDate: "postDate"
+  });
 });
 app.get("/signUp", (req, res) => {
-  res.render("boards/signUp");
+  res.render("boards/signUp", {
+    titlename: "SIGN UP"
+  });
 });
 
 // "/boards/:category"request "duck-craft/views/"+boards/category.html render
 app.get("/boards/:category", (req, res) => {
-  res.render("boards/" + req.params.category);
+  res.render("boards/" + req.params.category, {
+    titlename: req.params.category.toUpperCase(),
+    category: req.params.category.toUpperCase(),
+    postTitle: "postTitle",
+    postUser: "postUser",
+    postDate: "postDate",
+    postLike: "postLike"
+
+  });
 });
 // "/boards/:category/posts/new"request "duck-craft/views/"+boards/new_post.html render
 // category: humor , gossip
 app.get("/boards/:category/posts", (req, res) => {
-  res.render("boards/" + req.query.action);
+  res.render("boards/" + req.query.action, {
+    titlename: req.params.category.toUpperCase(),
+    category: req.params.category.toUpperCase(),
+    postTitle: "postTitle",
+    postUser: "postUser",
+    postDate: "postDate",
+    postLike: "postLike"
+  });
 });
 //"boards/:catogory/posts?action=new_post" =>"boards/"+req.query.action
 //"boards/:catogory/posts?action=edit_post" =? Same
